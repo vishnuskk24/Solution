ed8b7ae818921815f182775526f944213c8fe7df
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-07-17 23:45:45 +0530

    Implementing balanced Expression

---
+++ DataStructures/src/Stack/Activity2.java
@@ -0,0 +1,19 @@
+package Stack;
+
+public class Activity2 {
+
+	
+	public boolean isBalanced(String value) {
+		
+		if(value==null) {
+			throw new IllegalArgumentException();
+		}
+		return true;
+	}
+	
+	public static void main(String[] args){
+		
+		
+		
+	}
+}
