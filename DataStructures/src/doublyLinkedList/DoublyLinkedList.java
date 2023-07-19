dc944eae9792cadb5653bd54f572f1eb8dae68f9
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-07-19 12:50:09 +0530

    Node implemented in DoubleLinked List

--- DataStructures/src/doublyLinkedList/DoublyLinkedList.java
+++ DataStructures/src/doublyLinkedList/DoublyLinkedList.java
@@ -7,6 +7,59 @@ public class DoublyLinkedList <T>{
 		private Node previous;
 		T value;
 		private Node Next;
+		public Node(T value) {
+			this.value=value;
+		}
 		
 	}
+	
+	public Node head ;
+	public Node tail;
+	 
+	
+	// implementing Private methods
+	
+	
+//	getPrevious node
+//	getNode    - > it will return the node according to the index
+	
+	// implementing Public methods to use the Doubly Linked List data structures
+	public boolean isEmpty() {
+		return head==null;
+	}
+	
+	public void addFirst() {
+		if(isEmpty()) {
+			// no Node is present 
+			
+		}
+	}
+	
+	
+	
+	
+	
+	
+//	Opertions neeed to perform
+	
+//	isEmpty
+	//addFirst
+	//	Addlast
+	// print
+//	printWithIndex
+//	indexOf
+//	count
+//	contains
+//	size
+//	removeFirst
+//	removerLast
+//	toArray
+//	getMiddle
+//	hasLoop
+//	makeLoop
+//	BreakLoop
+//	addBefore
+//	addAfter
+//	deleteNode // deleting the value according to the index
+	
 }
