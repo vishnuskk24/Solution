4a4eeec5e18cff4f8611b9767d4072b1012bebc2
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-07-25 01:39:21 +0530

    Update pattern_22.java

--- TUF/src/patterns/pattern_22.java
+++ TUF/src/patterns/pattern_22.java
@@ -7,7 +7,21 @@ public class pattern_22 {
 	
 	
 	
-	public static void pettern(int n) {
+	public static void pattern(int n) {
+		
+		for(int i=0;i<2*n-1;i++) {
+			
+			for(int j=0 ;j<2*n-1;j++) {
+				
+				int top = i;
+				int down = j;
+				int left = (2*n-2) -i;
+				int right = (2*n-2)-j;
+				
+				System.out.print(n-Math.min(Math.min(left, right),Math.min(top, down)) + " ");
+			}
+			System.out.println();
+		}
 		
 	}
 
