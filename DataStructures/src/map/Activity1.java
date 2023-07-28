76e88a1fdabfefbf39e19b6c23ec43c54c4ec0d7
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-07-28 22:52:25 +0530

    finding first non repeative charcter prblm is completed

--- DataStructures/src/map/Activity1.java
+++ DataStructures/src/map/Activity1.java
@@ -1,5 +1,35 @@
 package map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Activity1 {
 
+	public static char findFirstNonRepetiveChracter(String input) {
+		
+		char result = 0;
+		Map<Character, Integer> pair = new HashMap<>();
+		
+		for(Character c : input.toCharArray()) {
+			
+			var count = pair.containsKey(c) ? pair.get(c) : 0;
+			
+			pair.put(c,count+1);
+			
+		}
+		
+		for(Character c:input.toCharArray()) {
+			if(pair.get(c)==1) {
+				return c;
+			}
+		}
+		
+		return result;
+		
+	}
+	public static void main(String[] args) {
+		
+		String input= "apple is read";
+		System.out.println(findFirstNonRepetiveChracter(input));
+	}
 }
