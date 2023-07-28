c7a60e032f53c5820defaae46e04f81fd698c1fd
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-07-28 22:44:43 +0530

    started one prblm

--- DataStructures/src/map/ArrayListMap.java
+++ DataStructures/src/map/ArrayListMap.java
@@ -1,5 +1,13 @@
 package map;
 
+import java.util.ArrayList;
+
 public class ArrayListMap<K,V>  implements Map<K, V>{
 
+	private ArrayList<K> key = new ArrayList<>();
+	private ArrayList<V> value  = new ArrayList();
+	
+	
+	
+	
 }
