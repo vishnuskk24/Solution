fa73061b0f0ebcf9189729c93514fc29f039d196
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-08-09 23:58:59 +0530

    started impleenting tree

---
+++ DataStructures/src/tree/BinaryTree.java
@@ -0,0 +1,24 @@
+package tree;
+
+public class BinaryTree<T> {
+
+	Node root;
+	
+	private class Node<T>{
+		
+		T value;
+		Node left;
+		Node right;
+		
+	}
+	
+	
+	// property only two child needs to be there
+	// left node needs to be lesser than current node and right node needs to be greater than current node value
+	
+	public void insert(T value) {
+		
+		
+	}
+	
+}
