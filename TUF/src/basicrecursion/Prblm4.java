d083cb775631243f33424e51a84a314c73ca8f84
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-08-12 16:55:07 +0530

    Create Prblm4.java

---
+++ TUF/src/basicrecursion/Prblm4.java
@@ -0,0 +1,21 @@
+package basicrecursion;
+
+public class Prblm4 {
+
+	 static void func(int i, int n){
+	        
+         // Base Condition.
+         if(i<1) return;
+         System.out.println(i);
+
+         // Function call to print i till i decrements to 1.
+         func(i-1,n);
+
+	 }
+	 public static void main(String[] args) {
+	
+	    // Here, let’s take the value of n to be 4.
+	    int n = 4;
+	    func(n,n);
+	 }
+}
