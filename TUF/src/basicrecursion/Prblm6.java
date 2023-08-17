ede03dcc07f1a8277aa3ca7c92acc470ba9892eb
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-08-17 23:50:58 +0530

    Create Prblm6.java

---
+++ TUF/src/basicrecursion/Prblm6.java
@@ -0,0 +1,23 @@
+package basicrecursion;
+
+public class Prblm6 {
+
+	
+	public static void main(String args[]) {
+	    int n = 5;
+	    if (n == 0) {
+	      System.out.println(0);
+	    } else {
+	      int fib[] = new int[n + 1];
+	      fib[0] = 0;
+	      fib[1] = 1;
+	      for (int i = 2; i <= n; i++) {
+	        fib[i] = fib[i - 1] + fib[i - 2];
+	      }
+	      System.out.println("The Fibonacci Series up to "+n+"th term:");
+	      for (int i = 0; i <= n; i++) {
+	        System.out.print(fib[i] + " ");
+	      }
+	    }
+	  }
+}
