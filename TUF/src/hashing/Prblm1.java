35ef97052476717f2ad23659db6e130fb7caf8d9
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-08-23 21:24:48 +0530

    Hashing

---
+++ TUF/src/hashing/Prblm1.java
@@ -0,0 +1,31 @@
+package hashing;
+
+public class Prblm1 {
+	public static void main(String args[]){  
+		   
+	      int arr[] ={10,5,10,15,10,5};
+	      int n = arr.length;
+	      countElement(arr, n);  
+	  }
+	public static void countElement(int arr[], int n)
+	{
+	    boolean visited[] = new boolean[n];
+	 
+	    for (int i = 0; i < n; i++) {
+	 
+	        // Skip this element if already processed
+	        if (visited[i] == true)
+	            continue;
+	 
+	        // Count frequency
+	        int count = 1;
+	        for (int j = i + 1; j < n; j++) {
+	            if (arr[i] == arr[j]) {
+	                visited[j] = true;
+	                count++;
+	            }
+	        }
+	        System.out.println(arr[i] + " " + count);
+	    }
+	}
+	}
