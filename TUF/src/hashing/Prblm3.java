0096863bdbaf06316d91b9abaa3146c5a225b591
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-08-24 22:48:34 +0530

    Create Prblm3.java

---
+++ TUF/src/hashing/Prblm3.java
@@ -0,0 +1,42 @@
+package hashing;
+
+public class Prblm3 {
+
+	public static void main(String args[]) {
+
+        int arr[] = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+        countFreq(arr, n);
+    }
+    public static void countFreq(int arr[], int n) {
+        boolean visited[] = new boolean[n];
+        int maxFreq = 0, minFreq = n;
+        int maxEle = 0, minEle = 0;
+        for (int i = 0; i < n; i++) {
+
+           
+            if (visited[i] == true)
+                continue;
+
+        
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            if (count > maxFreq) {
+                maxEle = arr[i];
+                maxFreq = count;
+            }
+            if (count < minFreq) {
+                minEle = arr[i];
+                minFreq = count;
+            }
+        }
+        System.out.println("The highest frequency element is: " + maxEle);
+        System.out.println("The lowest frequency element is: " + minEle);
+
+    }
+}
