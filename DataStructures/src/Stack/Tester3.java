cfc0994aa800376620d652e9bcae6cd5783063af
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-08-29 00:01:06 +0530

    two stacks in one array is done

--- DataStructures/src/Stack/Tester3.java
+++ DataStructures/src/Stack/Tester3.java
@@ -2,4 +2,24 @@ package Stack;
 
 public class Tester3 {
 
+	
+	public static void main(String[] args) {
+		TwoStack<Integer> stack = new TwoStack<Integer>(5);
+		
+		stack.push1(2);
+		stack.push1(4);
+		stack.push1(8);
+		stack.push2(16);
+		stack.push2(20);
+		System.out.println(stack.pop2()+ " +++++++++++++");
+//		stack.print2();
+		stack.printAll();
+		stack.push1(786);
+		System.out.println("------------");
+//		stack.printAll();
+//		stack.print1();
+		stack.print2();
+		stack.push2(3);
+	}
 }
+
--- DataStructures/src/Stack/TwoStack.java
+++ DataStructures/src/Stack/TwoStack.java
@@ -54,14 +54,19 @@ public class TwoStack<T> {
 		if(isEmpty1()) {
 			throw new NoSuchElementException("stack 1 is empty"); 
 		}
-		return this.arr[pointer1--];
+//		System.out.println("last element in stack one "  + this.arr[pointer1--]);
+		T rt = this.arr[pointer1-1];
+		this.pointer1--;
+		return rt;
 	
 	}
 	public T pop2() {
 		if(isEmpty2()) {
 			throw new NoSuchElementException("Stack 2 is Empty ");
 		}
-		return this.arr[pointer2++];
+		T ret = this.arr[pointer2+1];
+		pointer2++;
+		return ret;
 		
 		
 	}
@@ -75,21 +80,21 @@ public class TwoStack<T> {
 			return pointer2==arr.length-1;
 		}
 		public boolean isFull1() {
-			return pointer1== pointer2;
+			return pointer1-1>= pointer2;
 		}
 
 		public boolean isFull2() {
-			return pointer1== pointer2;
+			return pointer1-1==pointer2;
 		}
 		
 		
 		public void print1() {
-			for(int i =0;i<pointer1+1;i++) {
+			for(int i =0;i<pointer1;i++) {
 				System.out.println(this.arr[i]);
 			}
 		}
-		public void print() {
-			for(int i =arr.length-1;i>pointer2;i++) {
+		public void print2() {
+			for(int i =pointer2+1;i<arr.length;i++) {
 				System.out.println(this.arr[i]);
 			}
 		}
