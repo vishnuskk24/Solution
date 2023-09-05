eabd4250922b664af0259f7021bd27ce5baebd7b
vishnuskk24 <104623739+vishnuskk24@users.noreply.github.com>
2023-09-05 20:51:35 +0530

    changes done in insert method

--- DataStructures/src/tree/BinaryTree.java
+++ DataStructures/src/tree/BinaryTree.java
@@ -1,8 +1,12 @@
 package tree;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
-public class BinaryTree<T> {
+public class BinaryTree<T extends Comparable<T>>  {
 
 	private Node root;
+	private int size;
 	
 	private class Node<T>{
 		
@@ -10,27 +14,54 @@ public class BinaryTree<T> {
 		private Node left;
 		private Node right;
 		
+		
 		public Node(T value) {
 			this.value=value;
+			this.size=0;
 			
 		}
 	}
 	
-	private boolean isUserDefined(T value) {
-		
-		if(value instanceof Integer || value instanceof Double || value instanceof Long || value instanceof Character || value instanceof Boolean || value instanceof Float || value instanceof Byte  ) {
-		
-			return false;
-		}
-		return true;
-		
-	}
+//	private boolean isUserDefined(T value) {
+//		
+//		if(value instanceof Integer || value instanceof Double || value instanceof Long || value instanceof Character || value instanceof Boolean || value instanceof Float || value instanceof Byte  ) {
+//		
+//			return false;
+//		}
+//		return true;
+//		
+//	}
 	
 	
 	// property only two child needs to be there
 	// left node needs to be lesser than current node and right node needs to be greater than current node value
 	
+	private void insert(Node n, T value) {
+//			if compare to return value is positive then insertinf value is bigger choose right 
+		if(value.compareTo((T) n.value) > 0) {  // we did insert using recursion  there is an another option to use while loop
+			
+			if(n.right==null) {
+				this.size++;
+				n.right=new Node(value);
+				return;
+			}
+			insert(n.right,value);
+			
+		}else {
+			if(n.left==null) {
+				this.size++;
+				n.left= new Node(value);
+				return;
+			}
+			insert(n.left,value);
+		}
+	}
+	
+	public boolean isEmpty() {
+		return root==null;
+	}
 	public void insert(T value) {
+		
 		
 		Node node = new Node(value);
 		
@@ -38,32 +69,8 @@ public class BinaryTree<T> {
 			root= new Node(value);
 			return;
 			
-		}
-		var current = root;
 		
 		
-		while(true)
-		{
-//			if(!isUserDefined(value)) {
-				
-				if(value.hashCode()<current.value.hashCode()) {
-					
-					if(current.left==null) {
-						current.left=node;
-						break;
-					}
-					current=current.left;
-				}
-				else {
-					if(current.right==null) {
-						current.right=node;
-						break;
-					}
-					current=current.right;
-				}
-				
-				
-//			}
 		}
 		
 	}
