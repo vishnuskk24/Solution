be61efa33fe36e078a52e43df1a9a1dc0478a201
Vishnukumar M <104623739+vishnuskk24@users.noreply.github.com>
2023-10-14 16:44:19 +0530

    solved prblm 26

--- Leetcode/src/package2/Prblm26.java
+++ Leetcode/src/package2/Prblm26.java
@@ -1,5 +1,19 @@
 package package2;
 
 public class Prblm26 {
+//	https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3258/
+// Remove Duplicates from Sorted Array
+	 public int removeDuplicates(int[] nums) {
+	        int count=0;
+	        for(int i=0;i<nums.length;i++){
 
+	            if(nums[count] != nums[i]){
+	                count++;  
+	                nums[count] = nums[i];
+	                
+	                
+	            }
+	        }
+	        return count+1;
+	    }
 }
