47d80169bc5a57eb22515630c458ad5db9631855
Vishnukumar M <104623739+vishnuskk24@users.noreply.github.com>
2023-10-19 01:10:19 +0530

    Update Prblm21.java
    updated answer for Prblm21

--- Leetcode/src/package2/Prblm21.java
+++ Leetcode/src/package2/Prblm21.java
@@ -3,25 +3,28 @@ package package2;
 public class Prblm21 {
 //	https://leetcode.com/problems/merge-two-sorted-lists/
 //	merge two sorted linked list
-	var current=list2;
-    var editPointer=list1;
-    while(current!=null){
-        while(editPointer!=null && editPointer.next!=null){
-            if(current.val<=editPointer.next.val && current.val>editPointer.val){
-                editPointer.next   = new ListNode(current.val,editPointer.next );   //adding  current value in middle
 
-            }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+       if(list1==null) return list2;
+       if(list2==null) return list1;
+       if(list1.val> list2.val){
+           //swap
+           var temp = list1;
+           list1=list2;
+           list2=temp;
+       }
+ var result = list1;
+       while(list1!=null && list2!=null){
 
-            editPointer=editPointer.next;
-        }
-        ListNode n = new ListNode(current.val);
-        editPointer=n;
-        editPointer=editPointer.next;
-
-        current=current.next;
+           ListNode tmp = null;
+          while(list1!=null&&list1.val<=list2.val){
+              tmp=list1;
+              list1=list1.next;
+          }// iterate until the list1 it finds the list 2 next value greater
+            tmp.next = list2;
+            var t = list2;
+            list2=list1;
+            list1=t;
+       }
+       return result;
     }
-
-
-    
-    return list1;
-}
