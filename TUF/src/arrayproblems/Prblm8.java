03b8680036636a874e022e75b5ac58d1f981b5a0
Vishnukumar M <104623739+vishnuskk24@users.noreply.github.com>
2023-11-02 22:55:17 +0530

    solved Prblm268
    missing numbers

---
+++ TUF/src/arrayproblems/Prblm8.java
@@ -0,0 +1,17 @@
+package arrayproblems;
+
+public class Prblm8 {
+//	https://leetcode.com/problems/missing-number/submissions/
+		//Missing numbers
+	public int missingNumber(int[] nums) {
+        int N=nums.length;
+
+        int sum=(N*(N+1))/2;
+        int givenSum=0;
+        for(int i: nums){
+            givenSum+=i;
+            
+        }
+        return sum-givenSum;
+    }
+}
