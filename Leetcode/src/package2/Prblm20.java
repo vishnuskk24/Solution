9e05d9d8f75da11923d0db869468c9864e827659
Vishnukumar M <104623739+vishnuskk24@users.noreply.github.com>
2024-05-19 22:29:40 +0530

    Create Prblm20

---
+++ Leetcode/src/package2/Prblm20.java
@@ -0,0 +1,29 @@
+package package2;
+
+public class Prblm20 {
+
+public boolean isValid(String s) {
+        
+
+        Stack<Character> symbols =  new Stack();
+      
+        for(Character c : s.toCharArray()){
+           if(c=='{' || c=='[' || c=='('){  // if opening
+                symbols.push(c);
+           }else{
+            // closing tag
+            if(symbols.isEmpty()) return false;
+           
+            if(!symbols.isEmpty() && (symbols.peek() =='{' && c=='}' ) || (symbols.peek() =='(' && c==')' ) || (symbols.peek() =='[' && c==']' )){ 
+                
+                symbols.pop();
+            }else{
+                return false;
+            }
+           }
+       
+        }
+        
+        return symbols.isEmpty();
+    }
+}
