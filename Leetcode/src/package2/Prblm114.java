b9a3f58f40a93dc19cf1fe9338774e0d7779ee95
Vishnukumar M <104623739+vishnuskk24@users.noreply.github.com>
2024-05-23 18:18:16 +0530

    Create Prblm114.java

---
+++ Leetcode/src/package2/Prblm114.java
@@ -0,0 +1,47 @@
+package package2;
+
+public class Prblm114 {
+
+	
+	 List<TreeNode> nodes = new ArrayList();
+
+	    public void loadList(TreeNode tree){
+	        if(tree==null){
+	            return;
+	        }
+	        nodes.add(tree);
+	        loadList(tree.left);
+	        loadList(tree.right);
+	        
+	    }
+	    void printTree(TreeNode t){
+	        if(t==null){
+	            return;
+	        }
+	        System.out.println(t.val);
+	        printTree(t.left);
+	        printTree(t.right);
+	    }
+
+	    public void flatten(TreeNode root) {
+	        if(root==null || (root.left==null && root.right==null)){
+	            return;
+	        }
+	        loadList(root);
+	        // nodes.foreach((x)-> System.out.println(x.val));
+	        // for(TreeNode t : nodes){
+	        //     System.out.println(t.val);
+
+	        // }
+	        TreeNode current=root;
+	        // root.left=null;
+	        for(int i=1 ;i<nodes.size();i++){
+	            
+	            current.right = nodes.get(i);
+	            // System.out.println("adding - > " + current.val);
+	            current.left=null;
+	            current=current.right;
+	        }
+	        // printTree(root);
+	    }
+}
